d87812ae6576758c1cae009ad9c50b09deb01928
Ayush Sahu <ayushsahu1430@gmail.com>
2026-03-19 19:04:58 +0530

    Created a server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+    res.send('Hello');
+});
+
+app.use("/test", (req, res) => {
+    res.send('This is a test route.');
+});
+
+app.use("/", (req, res) => {
+    res.send('Welcome to the main route.');
+});
+
+app.listen(7777, () => {
+    console.log('Server is running on port 7777');
+});
