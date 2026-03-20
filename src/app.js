1d86d3658d60394226faa2c71ca4c0d914c5ec66
Ayush Sahu <ayushsahu1430@gmail.com>
2026-03-20 18:13:39 +0530

    Figuring out error handling

--- src/app.js
+++ src/app.js
@@ -1,27 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth")
-
-app.use("/admin", adminAuth);
-
-app.get("/user/login", (req, res) => {
-    res.send("User login");
-})
-
-app.get("/user", userAuth, (req, res) => {
-    res.send("User data");
-})
-
-app.get("/admin/getAllData", (req, res) => {
-    res.send("All data for admin");
+app.get("/getAllData", (req, res) => {
+  try {
+    throw new Error("Something went wrong while fetching data");
+  } catch (err) {
+    res.status(500).send("Something went wrong, please contact support");
+  }
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-    res.send("User deleted by admin");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(7777, () => {
-    console.log('Server is running on port 7777');
+  console.log("Server is running on port 7777");
 });
