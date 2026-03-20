8a2b4afc5921856e93bb055aef4ff90622992ef4
Ayush Sahu <ayushsahu1430@gmail.com>
2026-03-20 08:04:23 +0530

    Exploring GET, POST, DELETE API calls

--- src/app.js
+++ src/app.js
@@ -2,16 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send('Hello');
+app.get("/user", (req, res) => {
+    res.send({ firstname: "Ayush", lastname: "Sahu" });
+});
+
+app.post("/user", (req, res) => {
+    res.send("Data is being saved to the database.");
+});
+
+app.put("/user", (req, res) => {
+    res.send("Data is being updated in the database.");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data is being deleted from the database.");
+});
+
+app.patch("/user", (req, res) => {
+    res.send("Data is being partially updated in the database.");
 });
 
 app.use("/test", (req, res) => {
     res.send('This is a test route.');
-});
-
-app.use("/", (req, res) => {
-    res.send('Welcome to the main route.');
 });
 
 app.listen(7777, () => {
