b38687381857d7d38ab21aeef7484bfba54eba7c
Ayush Sahu <ayushsahu1430@gmail.com>
2026-03-20 08:37:43 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,28 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user", (req, res) => {
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params);
     res.send({ firstname: "Ayush", lastname: "Sahu" });
-});
-
-app.post("/user", (req, res) => {
-    res.send("Data is being saved to the database.");
-});
-
-app.put("/user", (req, res) => {
-    res.send("Data is being updated in the database.");
-});
-
-app.delete("/user", (req, res) => {
-    res.send("Data is being deleted from the database.");
-});
-
-app.patch("/user", (req, res) => {
-    res.send("Data is being partially updated in the database.");
-});
-
-app.use("/test", (req, res) => {
-    res.send('This is a test route.');
 });
 
 app.listen(7777, () => {
